e365f3421b6cb4f92a1bb03869332eafa7dd00fe
Daniel <daniel.sousa@germinare.org.br>
2025-07-27 19:59:25 -0300

    AddParadaFragment e MainitenceFragment layout feitos agora precisam de comportamento

--- mobile_hivemind/app/src/main/java/com/aula/mobile_hivemind/ui/paradas/AddParadaFragment.java
+++ mobile_hivemind/app/src/main/java/com/aula/mobile_hivemind/ui/paradas/AddParadaFragment.java
@@ -1,66 +1,52 @@
 package com.aula.mobile_hivemind.ui.paradas;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.aula.mobile_hivemind.R;
+import com.google.android.material.datepicker.MaterialDatePicker;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AddParadaFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class AddParadaFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public AddParadaFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddParadaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddParadaFragment newInstance(String param1, String param2) {
-        AddParadaFragment fragment = new AddParadaFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        // Construtor vazio obrigatório
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_parada, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        EditText editTextDATAPARADA = view.findViewById(R.id.editTextDATAPARADA);
+
+        editTextDATAPARADA.setOnClickListener(v -> {
+            MaterialDatePicker<Long> datePicker = MaterialDatePicker.Builder.datePicker()
+                    .setTitleText("Selecione a data")
+                    .build();
+
+            datePicker.show(getParentFragmentManager(), "tag");
+
+            datePicker.addOnPositiveButtonClickListener(selection -> {
+                SimpleDateFormat sdf = new SimpleDateFormat("dd, MMM yyyy", Locale.getDefault());
+                String selectedDate = sdf.format(new Date(selection));
+                editTextDATAPARADA.setText(selectedDate);
+            });
+        });
     }
 }
